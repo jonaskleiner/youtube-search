3ca5d5ebaf186d8bc4cf260f7e119b30543b4734
Jonas Kleiner <kleinerdesign@gmail.com>
2016-01-06 17:25:45 -0800

    retrieving data from YT API

---
+++ js/app.js
@@ -0,0 +1,18 @@
+$(function(){
+	console.log();
+});
+
+// show video search results with thumbnail images
+// get search results --> get data from YT
+$.ajax("https://www.googleapis.com/youtube/v3/search", {
+	data: {
+		part: "snippet",
+		key: "AIzaSyAwajfJSd0eNSi91UTVcLCLDAiewr_hdGU",
+		q: "surfing pug dogs"
+	}
+})
+.done(function(data){
+	console.log(data);
+});
+// for each search result --> loop through results
+// show title and thumbnail --> display the data in the UI
